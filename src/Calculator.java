4ffafc28b5515864601c73aaab68fd421e5f3aae
david <davidlsdlh14@gmail.com>
2026-03-04 23:19:17 +0100

    Refactorizacion 1: extraer metodos y usar libreria math

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,22 +1,23 @@
 
 public class Calculator {
 
+   
     public double calcular(double a, double b, String operacion) {
-        if (operacion.equals("suma")) {
-            return a + b;
-        } else if (operacion.equals("potencia")) {
-            double resultado = 1;
-            for (int i = 0; i < b; i++) {
-                resultado *= a;
-            }
-            return resultado;
-        } else if (operacion.equals("raiz")) {
-            double guess = a / 2;
-            for (int i = 0; i < 10; i++) {
-                guess = (guess + a / guess) / 2;
-            }
-            return guess;
-        }
-        return 0;
+    if (operacion.equals("suma")) {
+        return a + b;
+    } else if (operacion.equals("potencia")) {
+        return calcularPotencia(a, b);
+    } else if (operacion.equals("raiz")) {
+        return calcularRaiz(a);
     }
+    return 0;
+}
+
+private double calcularPotencia(double base, double exponente) {
+    return Math.pow(base, exponente);
+}
+
+private double calcularRaiz(double numero) {
+    return Math.sqrt(numero);
+}
 }
